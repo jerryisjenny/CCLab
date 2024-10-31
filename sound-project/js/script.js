126e539f7228b0aad73e9a32a6eaef702129806e
jerryisjenny <jt4993@nyu.edu>
2024-10-31 15:58:33 +0800

    change

---
+++ sound-project/js/script.js
@@ -0,0 +1,24 @@
+let sound;
+function preload() {
+    sound = loadSound("assets/song.mp3");
+}
+
+function setup() {
+    let canvas = createCanvas(500, 400);
+    canvas.parent("p5-canvas-container");
+}
+
+function draw() {
+    background(0);
+    // let vol = map(mouseY, 0, height, 0.0, 1.0, true);
+    // sound.setVolume(vol);
+
+    let panVal = map(mouseX, 0, width, 1.0, -1.0, true);
+    sound.pan(panVal);
+    let rateVal = map(mouseY, 0, height, 0.1, 1.5, true);
+    sound.rate(rateVal);
+}
+
+function mouseClicked() {
+    sound.play();
+}
